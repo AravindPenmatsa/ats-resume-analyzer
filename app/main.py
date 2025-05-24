47cca366598242a51666991d2a200f967b4abe84
Aravind Penmatsa <aravind.raju541@gmail.com>
2025-05-24 16:56:27 -0500

    Add GET /upload route for upload form

--- app/main.py
+++ app/main.py
@@ -1,24 +1,25 @@
 
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import HTMLResponse
-from app.routes import router as resume_router
-from app.download_route import router as download_router
+from app.routes import router
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="templates"), name="static")
-app.include_router(resume_router)
-app.include_router(download_router)
+app.include_router(router)
 
 @app.get("/", response_class=HTMLResponse)
-async def root():
-    return '''
-    <html>
-        <head><title>Resume Analyzer</title></head>
-        <body style="font-family:sans-serif; text-align:center; padding-top:50px;">
-            <h2>✅ Resume Analyzer App is Live</h2>
-            <p><a href="/upload">Click here to upload a resume</a></p>
-        </body>
-    </html>
-    '''
+def homepage():
+    return """
+    <h2>✅ Resume Analyzer App is Live</h2>
+    <a href='/upload'>Click here to upload a resume</a>
+    """
+
+@app.get("/upload", response_class=HTMLResponse)
+def upload_page():
+    return """
+    <h3>Upload Your Resume (TXT or PDF):</h3>
+    <form action="/evaluate" method="post" enctype="multipart/form-data">
+        Resume: <input type="file" name="resume"><br><br>
+        Job Description: <input type="file" name="jobdesc"><br><br>
+        <input type="submit" value="Evaluate">
+    </form>
+    """
