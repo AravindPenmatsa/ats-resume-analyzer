b4f6823a0a399c4b14b4774de5555da1d56a923c
Aravind Penmatsa <aravind.raju541@gmail.com>
2025-05-24 17:21:41 -0500

    Fix: updated evaluate button results

--- app/main.py
+++ app/main.py
@@ -1,18 +1,34 @@
-
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.routes import router
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
-app.include_router(router)
-
 templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/", response_class=HTMLResponse)
-async def homepage(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/upload", response_class=HTMLResponse)
-async def upload_page(request: Request):
+async def show_upload_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload")
+async def handle_upload(
+    request: Request,
+    resume: UploadFile = File(...),
+    jobdesc: UploadFile = File(...)
+):
+    resume_contents = await resume.read()
+    jobdesc_contents = await jobdesc.read()
+
+    return templates.TemplateResponse("result.html", {
+        "request": request,
+        "resume_name": resume.filename,
+        "jobdesc_name": jobdesc.filename,
+        "message": "Files uploaded successfully!"
+    })
